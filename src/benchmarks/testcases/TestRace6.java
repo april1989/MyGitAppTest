0979a096e9a73804ca56b28a5b87b2c72a9433ce
Bozhen Liu <april1989@tamu.edu>
2021-01-19 12:38:59 -0600

    code back

--- src/benchmarks/testcases/TestRace6.java
+++ src/benchmarks/testcases/TestRace6.java
@@ -46,7 +46,7 @@ public class TestRace6 {
             }
         };
         t1.start();
-        synchronized (lock) { 
+        synchronized (lock2) { 
             x++; 
         } 
         System.out.println("x = " + x);  
